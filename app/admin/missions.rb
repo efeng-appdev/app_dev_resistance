863bfe1c29ce9239319fd8db788260caf463cb58
Raghu Betina <raghu@firstdraft.com>
2018-03-13 00:39:04 +0000

    Whitelist columns in Mission for ActiveAdmin

--- app/admin/missions.rb
+++ app/admin/missions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Mission do
+
+ permit_params :total_passes, :total_fails, :outcome, :total_approve, :total_disapprove, :mission_approved
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
