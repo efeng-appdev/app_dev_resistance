1944d8b1b8a9eca060130957d4a79c64711961e6
Raghu Betina <raghu@firstdraft.com>
2018-03-13 00:38:47 +0000

    Direct association between users and mission_votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :mission_votes,
+             :dependent => :destroy
+
   has_many   :mission_participants,
              :dependent => :destroy
 
